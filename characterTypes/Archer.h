c22e1a1a4ff28ad269255aa4823abc1ff0a36176
AmandeepSingh180d <asing241@ucr.edu>
2024-02-19 16:47:28 -0800

    Added both the .h and .cpp of the Archer class

---
+++ characterTypes/Archer.h
@@ -0,0 +1,26 @@
+#ifndef ARCHER_H
+#define ARCHER_H
+
+#include <vector>
+#include <string>
+#include <random>
+#include "Character.h"
+
+class Archer : public Character {
+    private:
+        int specialPoints;
+        int health;
+        std::vector<std::string> Inventory = {"","","",""};
+        std::vector<std::string> listOfInventory = {"SP refill", "HealthUp","AtackUp","Small potion", "Stew","DefenseU"};
+
+    public:
+        virtual int getHealth();
+        virtual void setHealth(int newHealth);
+        virtual int getSpecialPoints();
+        virtual void setSpecialPoints(int newSP);
+        virtual std::vector<std::string> getInventory();
+        virtual void setInventory(std::vector<std::string> chosenInventory);
+        void randomizer();
+};
+
+#endif // ARCHER_H
