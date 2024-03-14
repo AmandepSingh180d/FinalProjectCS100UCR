8b46c47893704be021e835a46308881918153322
AmandeepSingh180d <asing241@ucr.edu>
2024-03-14 08:55:42 -0700

    fixed error with attackTest

--- tests/testsAttackClass/testAttack.cpp
+++ tests/testsAttackClass/testAttack.cpp
@@ -7,29 +7,45 @@ TEST(AttackTest, testBasicAttack){
     EXPECT_EQ(attack.basicAttack(), 10);
 }
 
-// Test the specialAttack method - test insufficient SP
-TEST(AttackTest, testInsufficientSpecialAttack) {
-    Attack attack;
-    EXPECT_EQ(0, attack.specialAttack(4, 1));
-    EXPECT_EQ(0, attack.specialAttack(9, 2));
-    EXPECT_EQ(0, attack.specialAttack(14, 3));
+TEST(AttackTest, testArcherAttack) {
+    archerAttack aAtk;
+    EXPECT_EQ(10,aAtk.basicAttack());
+    EXPECT_EQ(0, aAtk.specialAttack(4, 1,10));
+    EXPECT_EQ(0, aAtk.specialAttack(9, 2,10));
+    EXPECT_EQ(0, aAtk.specialAttack(14, 3,10));
+    EXPECT_EQ(30, aAtk.specialAttack(5, 1,10));
+    EXPECT_EQ(40, aAtk.specialAttack(10, 2,10));
+    EXPECT_EQ(50, aAtk.specialAttack(15, 3,10));
+}
+TEST(AttackTest, testKnightAttack) {
+    knightAttack kAtk;
+    EXPECT_EQ(10, kAtk.basicAttack());
+    EXPECT_EQ(0, kAtk.specialAttack(4, 1, 10));
+    EXPECT_EQ(0, kAtk.specialAttack(9, 2, 10));
+    EXPECT_EQ(0, kAtk.specialAttack(14, 3, 10));
+    EXPECT_EQ(30, kAtk.specialAttack(5, 1, 10));
+    EXPECT_EQ(40, kAtk.specialAttack(10, 2, 10));
+    EXPECT_EQ(50, kAtk.specialAttack(15, 3, 10));
 }
 
-//test the specialAttack method - test sufficient SP/passing test
-TEST(AttackTest, testSufficientSpecialAttack){
-    Attack attack;
-    EXPECT_EQ(20, attack.specialAttack(5, 1));
-    EXPECT_EQ(30, attack.specialAttack(10, 2));
-    EXPECT_EQ(40, attack.specialAttack(15, 3));
-    EXPECT_EQ(20, attack.specialAttack(8, 1)); 
-    EXPECT_EQ(30, attack.specialAttack(12, 2)); 
-    EXPECT_EQ(40, attack.specialAttack(18, 3));
+TEST(AttackTest, testMageAttack) {
+    mageAttack mAtk;
+    EXPECT_EQ(10, mAtk.basicAttack());
+    EXPECT_EQ(0, mAtk.specialAttack(4, 1, 10));
+    EXPECT_EQ(0, mAtk.specialAttack(9, 2, 10));
+    EXPECT_EQ(0, mAtk.specialAttack(14, 3, 10));
+    EXPECT_EQ(30, mAtk.specialAttack(5, 1, 10));
+    EXPECT_EQ(40, mAtk.specialAttack(10, 2, 10));
+    EXPECT_EQ(50, mAtk.specialAttack(15, 3, 10));
 }
 
-//test the specialAttack method - test invalid choice
-TEST(AttackTest, testInvalidChoiceAttack){
-    Attack attack;
-    EXPECT_EQ(0, attack.specialAttack(4, 3));
-    EXPECT_EQ(0, attack.specialAttack(9, 4));
-    EXPECT_EQ(0, attack.specialAttack(14, 2));
+TEST(AttackTest, testThiefAttack) {
+    thiefAttack tAtk;
+    EXPECT_EQ(10, tAtk.basicAttack());
+    EXPECT_EQ(0, tAtk.specialAttack(4, 1, 10));
+    EXPECT_EQ(0, tAtk.specialAttack(9, 2, 10));
+    EXPECT_EQ(0, tAtk.specialAttack(14, 3, 10));
+    EXPECT_EQ(30, tAtk.specialAttack(5, 1, 10));
+    EXPECT_EQ(40, tAtk.specialAttack(10, 2, 10));
+    EXPECT_EQ(50, tAtk.specialAttack(15, 3, 10));
 }
